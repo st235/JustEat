3eeff04ee14ef4d653d82e83ce52279a14e77afc
Alexander Dadukin <hi@st235.xyz>
2023-03-12 13:10:59 +0000

    add progress indicator

--- app/src/main/java/com/github/st235/justeat/presentation/MainActivity.kt
+++ app/src/main/java/com/github/st235/justeat/presentation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.github.st235.justeat.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.st235.justeat.BuildConfig
@@ -19,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var restaurantsRecyclerView: RecyclerView
     private lateinit var postcodeInputEditText: TextInputEditText
+    private lateinit var progressIndicator: ProgressBar
+    private lateinit var locationButton: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,17 +30,21 @@ class MainActivity : AppCompatActivity() {
 
         restaurantsRecyclerView = findViewById(R.id.recycler_view)
         postcodeInputEditText = findViewById(R.id.postcode_input_edit_text)
+        progressIndicator = findViewById(R.id.loading_indicator)
+        locationButton = findViewById(R.id.location_button)
 
         restaurantsRecyclerView.layoutManager = LinearLayoutManager(this)
         restaurantsRecyclerView.adapter = restaurantsAdapter
 
         mainViewModel.observeRestaurants()
             .observe(this) { restaurants ->
+                progressIndicator.visibility = View.INVISIBLE
                 restaurantsAdapter.setItems(restaurants)
             }
 
         mainViewModel.observeErrors()
             .observe(this) { throwable ->
+                progressIndicator.visibility = View.INVISIBLE
                 Log.e("HelloWorld", "Exception", throwable)
             }
 
@@ -46,10 +54,11 @@ class MainActivity : AppCompatActivity() {
     private inner class PostcodeInputTextWatcher: DelayedTextWatcher(BuildConfig.DELAY_INPUT_POSTCODE) {
 
         override fun onDelayedTextChange(text: String?, delay: Long) {
-            if (text == null) {
+            if (text.isNullOrEmpty()) {
                 return
             }
 
+            progressIndicator.visibility = View.VISIBLE
             mainViewModel.findRestaurantsByPostcode(text)
         }
     }
